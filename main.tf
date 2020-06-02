7cb10ef65188ba05bf43981d50b4ea6cbdde43d6
22pop.fm <panpakapan0326@gmail.com>
2020-06-02 16:14:34 +0900

    comment out

--- main.tf
+++ main.tf
@@ -8,33 +8,33 @@ data "aws_iam_policy_document" "allow_describe_regions" {
 }
 
 # IAMポリシーのリソースを作成
-resource "aws_iam_policy" "example" {
-  name   = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
+# resource "aws_iam_policy" "example" {
+#   name   = "example"
+#   policy = data.aws_iam_policy_document.allow_describe_regions.json
+# }
 
-# IAMロール（信頼）の内容を記載　EC2にのみ関連付けすることができる
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
+# # IAMロール（信頼）の内容を記載　EC2にのみ関連付けすることができる
+# data "aws_iam_policy_document" "ec2_assume_role" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ec2.amazonaws.com"]
+#     }
+#   }
+# }
 
-# IAMロールを作成
-resource "aws_iam_role" "example" {
-  name               = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-}
+# # IAMロールを作成
+# resource "aws_iam_role" "example" {
+#   name               = "example"
+#   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+# }
 
-# IAMポリシーとIAMロールを紐付けて有効にする
-resource "aws_iam_role_policy_attachment" "example" {
-  role       = aws_iam_role.example.name
-  policy_arn = aws_iam_policy.example.arn
-}
+# # IAMポリシーとIAMロールを紐付けて有効にする
+# resource "aws_iam_role_policy_attachment" "example" {
+#   role       = aws_iam_role.example.name
+#   policy_arn = aws_iam_policy.example.arn
+# }
 
 
 
