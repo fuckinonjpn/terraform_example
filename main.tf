f55457f736cc8966a426b8ebbc16db97ac1505f6
22pop.fm <panpakapan0326@gmail.com>
2020-06-02 19:08:45 +0900

    strage完成

--- main.tf
+++ main.tf
@@ -44,3 +44,33 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+resource "aws_s3_bucket" "private" {
+  # 全世界で一意性制約
+  bucket = "private-promatic-terraform"
+  # 以下は追加のデメリット無し
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# S3のパブリックアクセス防止
+resource "aws_s3_bucket_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl    = "public-read"
+}
